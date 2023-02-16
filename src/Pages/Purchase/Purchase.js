2d62f5871a71af4cd781a71e6fe76bbe7497e116
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-02-17 00:34:17 +0600

    added purchase

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -16,24 +16,28 @@ const Purchase = () => {
 
   return (<div>
     {product &&  <div className="pt-12 bg-slate-300 ">
-      <div className="md:flex ">
-      <img src={img} alt="" srcSet="" className="w-96 " />
-      <div>
+      <div className="md:flex justify-center items-center">
+        <div className="w-1/2 text-right">
+      <img src={img} alt="" srcSet="" className=" w-full" />
+        </div>
+      <div className="w-1/2">
         <h1 className="text-5xl">{name}</h1>
         <br />
         <p className="text-justify">{description}</p>
         <br />
-        <div className="">
+        <div className=" flex gap-8">
         <span className="flex items-center w-32 text-primary rounded px-2 border border-slate-400 text-xl bg-slate-300 ">
-          <AiOutlineMinus className="mr-4"/>
-        <span className="font-semibold ">{orderQty}</span>
-        <AiOutlinePlus className="ml-4"/>
+          <AiOutlineMinus className="mr-3"/>
+        <span className="font-semibold ml-2">{orderQty}</span>
+        <AiOutlinePlus className="ml-2 border-r-primary border-1"/>
         </span>     
 
         <span className=" text-primary rounded w-32 px-4 border border-slate-400 text-lg bg-slate-300 "><span className="text-slate-600">Price</span> <span className="font-semibold">${price}</span>
-        </span>     
+        </span>   
 
         </div>
+ <br />
+        <button className="btn btn-primary">ADD TO CART</button>  
       </div>
       </div>   
       </div>}
---
+++ src/api/productAPI.js
@@ -0,0 +1,10 @@
+// import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+// export const productApi = createApi({
+//     baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000/"}),
+//     endpoints: (builder)=>({
+//         products: builder.query<[], void>({
+
+//         })
+//     })
+// })
