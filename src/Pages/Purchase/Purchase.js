3e4909617e711b7e96321a0563374c4a8a6d1c95
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-02-20 01:15:33 +0600

    add related products

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import { useProductQuery } from '../../api/productAPI.ts';
 import Button from '../../component/share/Button';
+import RelatedProducts from './RelatedProducts';
 
 const Purchase = () => {
   const [orderQty, setOrderQty]= useState(1)
@@ -39,27 +40,27 @@ const handleDecreaseOrderQty = () =>{
   return (<div>
     {error && <p className="text-red-600">{error.status}</p>}
     {product  && <div className="pt-6  ">
-      <div className="md:flex justify-center items-center">
-        <div className="w-1/2 text-right ">
-      <img src={product.img} alt="" srcSet="" className=" mx-auto w-3/5" />
+      <div className="px-5 md:flex justify-center items-center">
+        <div className="rounded-full md:w-1/2 ">
+      <img src={product.img} alt="" srcSet="" className=" mx-auto w-[70vh]" />
         </div>
-      <div className="w-1/2 text-left ">
-        < h1 className="text-5xl font-bold mb-6"> {product.name} </h1>
-        <div className="w-1/2">
+      <div className="w-full md:w-1/2 text-left ">
+        < h1 className="text-center text-5xl font-bold mb-6"> {product.name} </h1>
+        <div className="mx-auto w-full md:w-1/2">
         <p className="text-justify">{product.description}</p>
         <br />
-        <div className=" flex ">
+        <div className=" flex mb-10">
         <div className="flex items-center w-1/2 text-primary rounded border border-slate-400 text-xl bg-slate-300 mr-3 ">
           <AiOutlineMinus
         onClick={()=>handleDecreaseOrderQty()}
           
-          className=" w-12 px-2 mr-3 border-r-2 border-slate-400 h-full hover:bg-slate-400 cursor-pointer"/>
-        <span className="font-semibold w-10 text-center mx-4 px-1">{orderQty}</span>
+          className="w-14 md:w-12 px-4 md:px-2 mr-6 md:mr-3 border-r-2 border-slate-400 h-full hover:bg-slate-400 cursor-pointer"/>
+        <span className="font-semibold w-12 md:w-10 text-center md:mx-4 px-1">{orderQty}</span>
         <AiOutlinePlus
         onClick={()=>handleIncreaseOrderQty()}
-        className=" w-12 px-2 ml-3 border-l-2 border-slate-400 h-full hover:bg-slate-400 cursor-pointer"/>
+        className="w-14 md:w-12 px-4 md:px-2 ml-6 md:ml-3 border-l-2 border-slate-400 h-full hover:bg-slate-400 cursor-pointer"/>
         </div>     
-        <div className=" text-primary rounded w-1/2 px-4 border border-slate-400 text-lg bg-slate-300 ml-3 ">
+        <div className=" text-primary text-center rounded w-1/2 px-4 border border-slate-400 text-lg bg-slate-300 ml-3 ">
           <span className="text-slate-600">Price &nbsp; : </span>
           <span className="font-semibold">${product.price}</span>
         </div>   
@@ -69,11 +70,11 @@ const handleDecreaseOrderQty = () =>{
         <Button>
         ADD TO CART
         </Button>
-
         </div>
  <br />
       </div>
       </div>   
+      <RelatedProducts />
       </div>}
   </div>    
   )
---
+++ src/Pages/Purchase/RelatedProducts.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Button from '../../component/share/Button'
+
+const RelatedProducts = () => {
+
+  const rating =  <div className="flex items-center mt-2.5 mb-5">
+            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+        </div>
+  return (
+    <div>
+      <h1 className="text-2xl text-center">Related <span className="text-primary">Products</span></h1>
+      
+<div className="w-full text-secondary my-5 mx-auto max-w-sm  border  rounded-lg shadow  border-gray-700">
+    <a href="#" >
+        <img className="px-10 rounded-t-lg" src="https://i.ibb.co/vHhRwHr/c3.png" alt="product image" />
+    </a>
+    <div className="px-5 pb-5">
+        <a href="#" >
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+        </a>
+        {rating}
+            <p className="text-3xl">Price &nbsp; : <span className=" text-primary ">$599</span></p>      
+            <br />            
+            {/* <NavLink to={`/purchase/${product._id}`} className=""> */}
+            <NavLink to={`/purchase/63e923afffc07097ee3a59d3`} className="">
+      <Button>
+        Add To Cart
+      </Button>
+    </NavLink>      
+    </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default RelatedProducts
